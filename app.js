c7d567ff8c9f172a22aaf5cba25a507fd8b6d2fa
mspake <morgana.spake@gmail.com>
2019-03-23 11:29:05 -0700

    switched to spheres rendering in 3D with a randomized x,y,z

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 // ellipseMode(RADUIS);
 
-let x = 0;
-
 var new_dot_one = randomize();
 var new_dot_two = randomize();
 var new_dot_three = randomize();
 
 
 
-
 function setup() {
-  var cnv = createCanvas(windowWidth, windowHeight);
+  var cnv = createCanvas(windowWidth, windowHeight, WEBGL);
   background(0, 0, 0);
 }
 
@@ -20,18 +17,34 @@ function windowResized() {
 }
 
 function draw() {
-  fill(255);
-  ellipse(new_dot_one[0], new_dot_one[1], 10, 10);
-  ellipse(new_dot_two[0], new_dot_two[1], 10, 10);
-  ellipse(new_dot_three[0], new_dot_three[1], 10, 10);
+  ambientMaterial(250);
+  directionalLight(255, 255, 255, 0, 1, -2);
+  noStroke();
+
+  push();
+  translate(new_dot_one[0], new_dot_one[1], new_dot_one[2]);
+  sphere(10);
+  pop();
+
+  push();
+  translate(new_dot_two[0], new_dot_two[1], new_dot_two[2]);
+  sphere(10);
+  pop();
+
+  push();
+  translate(new_dot_three[0], new_dot_three[1], new_dot_three[2]);
+  sphere(10);
+  pop();
+
 }
 
 
 function randomize() {
-  var randomX = Math.round((Math.random() * (window.innerWidth - 20)) + 10);
-  var randomY = Math.round((Math.random() * (window.innerHeight - 20)) + 10);
-  var coordinates = [randomX, randomY];
+  var randomX = Math.floor(((Math.random() * (window.innerWidth - 20)) - (window.innerWidth / 2) + 10));
+  var randomY = Math.floor(((Math.random() * (window.innerHeight - 20)) - (window.innerHeight / 2) + 10));
+  var randomZ = Math.floor(Math.random() * -100);
+
+  var coordinates = [randomX, randomY, randomZ];
   console.log(coordinates);
-  // randomZ = Math.round(Math.random() * 10);
   return coordinates;
 }
