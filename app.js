37ac1fe75554361809ba9c3e6137414a3a4367ef
mspake <morgana.spake@gmail.com>
2019-03-22 18:39:28 -0700

    p5.js library CDN linked and working

---
+++ app.js
@@ -0,0 +1,11 @@
+ellipseMode(RADUIS);
+
+function setup() {
+  var cnv = createCanvas(windowWidth, windowHeight);
+  background(0, 0, 0);
+}
+
+function draw() {
+  fill(255);
+  ellipse(200, 200, 10, 10);
+}
